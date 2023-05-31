32e91110603f3cbf4f1f91d76ad08c7f473816d6
Christopher Osgood <53442357+xosgood@users.noreply.github.com>
2023-05-31 11:35:37 -0700

    Add measurements and EKF outline

--- SimulateGroundTruth.m
+++ SimulateGroundTruth.m
@@ -8,6 +8,7 @@ rng(273);
 n_L = 3; % number of dimensions of state of leader
 n_F = 3; % number of dimensions of state of follower
 p = 2; % number of dimensions of control input
+m = 3; % number of dimensions of measurement (leader's measurement of follower)
 
 dt = 0.1;
 t_f = 200;
@@ -16,6 +17,7 @@ N = length(tspan);
 
 Q = 0.001 * eye(n_L);
 Q_rel = 0.2 * eye(n_F);
+R = diag([1, 0.5, 0.5]);
 
 v = 10 * ones(1,N); % velocity command
 omega = sin(tspan/3); % angular velocity command
@@ -27,34 +29,48 @@ omega_follower_max_thresh = 0.05;
 
 K_p = [.1; .01] * 10 * sin(5*tspan); % proportional gain for control loop for follower
 
+% creating vectors to hold data
 x_L = zeros(n_L, N); % true leader state, through simulating dynamics
 x_F_des = zeros(n_F, N); % desired follower state, through simulating dynamics
 x_F_act = zeros(n_F, N); % actual follower state
+y = zeros(m, N-1); % measurements
 
+
+% initial conditions
 x_L(:,1) = zeros(n_L, 1);
 x_F_des(:,1) = [-10; -10; deg2rad(0)];
 x_F_act(:,1) = [-20; -20; deg2rad(45)];
 
+
 %% simulate dynamics
 for i = 2:N
+    % leader dynamics
     w = mvnrnd(zeros(n_L,1), Q)'; % process noise
-    w_rel = mvnrnd(zeros(n_F,1), Q_rel)'; % process noise
     x_L(:,i) = f_abs(x_L(:,i-1), u_L(:,i-1), dt) + w; % leader dynamics propagation
     
+    % follower dynamics
+    w_rel = mvnrnd(zeros(n_F,1), Q_rel)'; % process noise
     x_F_des(:,i) = f_rel(x_F_des(:,i-1), [0; 0], dt); % desired follower dynamics propagation
     x_F_act(:,i) = f_rel(x_F_act(:,i-1), u_F(:,i-1), dt) + w_rel; % actual follower dynamics propagation
     
+    % control law for follower
     e = x_F_act(:,i-1) - x_F_des(:,i-1);
     e_x = e(1);
     e_y = e(2);
     e_psi = e(3);
     e_rho = norm(e(1:2));
     Beta = atan2(e_y, e_x);
-    
     u_F(:,i) = K_p(:,i) .* [e_rho;
                             e_psi - Beta];
     u_F(1,i) = sign(u_F(1,i)) * min(abs(u_F(1,i)), v_follower_max_thresh);
     u_F(2,i) = sign(u_F(2,i)) * min(abs(u_F(2,i)), omega_follower_max_thresh);
+    
+    % measure (leader's measurments of followers)
+    v = mvnrnd(zeros(m,1), R)'; % measurement noise
+    y = g(x_F_act(:,i)) + v; % measure
+    
+    % EKF
+    
 end
 
 x_F_des_global = x_L + x_F_des;
@@ -94,7 +110,7 @@ end
 
 % nonlinear measurement
 function y = g(x)
-    y = norm(x(1:2));
+    y = ;
 end
 
 % desired relative dynamics
